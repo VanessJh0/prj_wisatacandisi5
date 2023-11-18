d95434127faf06cd07a2c50685f5bb0c1b044199
fadfasdasf <126873014+VanessJh0@users.noreply.github.com>
2023-11-18 18:17:11 +0700

    Today
    Done

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:prj_wisatacandisi51/widgets/home_screen.dart';
 import 'package:prj_wisatacandisi51/widgets/profile_screen.dart';
 import 'package:prj_wisatacandisi51/widgets/search_screen.dart';
 import 'package:prj_wisatacandisi51/widgets/signin_screen.dart';
+import 'package:prj_wisatacandisi51/widgets/favorite_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,11 +37,78 @@ class MyApp extends StatelessWidget {
         //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         //   useMaterial3: true,
         // ),
-        home: HomeScreen()
+        home: MainScreen()
         //SearchScreen(),
         //SignInScreen()
         //ProfileScreen(),
         //DetailScreen(candi: candiList[0]),
         );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    HomeScreen(),
+    SearchScreen(),
+    FavoriteScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_currentIndex],
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(canvasColor: Colors.blueAccent[50]),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (value) {
+            setState(() {
+              _currentIndex = value;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                  color: Colors.blueAccent,
+                ),
+                label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.blueAccent,
+                ),
+                label: "Pencarian"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.favorite,
+                  color: Colors.blueAccent,
+                ),
+                label: "Favorite"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.blueAccent,
+                ),
+                label: "Profile"),
+          ],
+          selectedItemColor: Colors.blueAccent,
+          unselectedItemColor: Colors.blueAccent[100],
+          showUnselectedLabels: true,
+        ),
+      ),
+      //TODO 2 Buat Properti body berupa widget yg ditampilkan
+      //TODO 3 Buat Properti bottomNavigatorBar dengan nilai Theme
+      //TODO 4 Buat Buat data dan child dari Theme
+    );
   }
 }
